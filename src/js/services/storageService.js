27fbfa8771af2a4cf2209dbfebba7dc962d5255a
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-04-21 16:03:43 +0500

    feat: add getter for current period

--- src/js/services/storageService.js
+++ src/js/services/storageService.js
@@ -2,9 +2,22 @@ import seedData from "../../data/appData.json";
 import { STORAGE_KEY, state } from "../state/appState.js";
 import { deepClone } from "../utils/format.js";
 
+function toPeriodKey(year = state.currentYear, month = state.currentMonth) {
+  return `${year}-${month}`;
+}
+
+function getCurrentPeriodData() {
+  const key = toPeriodKey();
+  if (!state.monthlyData[key]) {
+    state.monthlyData[key] = { employees: [], projects: [] };
+  }
+  return state.monthlyData[key];
+}
+
 function saveData() {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(state.monthlyData));
 }
+
 
 function loadData() {
   state.monthlyData = deepClone(seedData);
@@ -12,7 +25,4 @@ function loadData() {
   saveData();
 }
 
-export {
-  saveData,
-  loadData,
-};
+export { toPeriodKey, getCurrentPeriodData, saveData, loadData };
