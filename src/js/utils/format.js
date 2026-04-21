ccda8dda3f782763af25228f9b18d43670ae50db
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-04-21 19:20:01 +0500

    feat: add format currency

--- src/js/utils/format.js
+++ src/js/utils/format.js
@@ -2,8 +2,17 @@ function deepClone(value) {
   return JSON.parse(JSON.stringify(value));
 }
 
+function formatCurrency(value) {
+  return Number(value || 0).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
+
 function toFixed(value, digits = 2) {
   return Number(value || 0).toFixed(digits);
 }
 
-export { deepClone, toFixed };
+export { deepClone, formatCurrency, toFixed };
