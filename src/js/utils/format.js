a84fe6d5097aa5254b66f7bb125623f4516a28e1
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-04-21 17:00:36 +0500

    feat: add toFixed helper

--- src/js/utils/format.js
+++ src/js/utils/format.js
@@ -2,4 +2,8 @@ function deepClone(value) {
   return JSON.parse(JSON.stringify(value));
 }
 
-export { deepClone };
+function toFixed(value, digits = 2) {
+  return Number(value || 0).toFixed(digits);
+}
+
+export { deepClone, toFixed };
